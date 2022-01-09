5395bc44aea38cdb313ecb99ea3020a55ce1b88e
aperezdev98 <adrianperez.dev@gmail.com>
2022-01-09 12:19:30 -0500

    Initial Commit

---
+++ Project1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project1
+//
+//  Created by Adrian Perez on 1/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project1/Project1App.swift
@@ -0,0 +1,17 @@
+//
+//  Project1App.swift
+//  Project1
+//
+//  Created by Adrian Perez on 1/9/22.
+//
+
+import SwiftUI
+
+@main
+struct Project1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
